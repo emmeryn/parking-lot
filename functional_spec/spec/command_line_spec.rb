993304df92252b3be7434c06031510f44d82b8a9
Sue Zheng Hao <szhlibrary@gmail.com>
2020-01-19 02:01:30 +0800

    Specs/Command: Prefer active verbs in test descriptions.

--- functional_spec/spec/command_line_spec.rb
+++ functional_spec/spec/command_line_spec.rb
@@ -7,7 +7,10 @@ RSpec.describe 'Command Line Functions' do
     run_command(pty, "create_parking_lot 3\n") unless test.metadata[:skip_parking_lot_init]
   end
 
-  it "should fail to create a parking lot with invalid slot number", :skip_parking_lot_init do
+  it "fails to create a parking lot given invalid slot number", :skip_parking_lot_init do
+    run_command(pty, "create_parking_lot 0\n")
+    expect(fetch_stdout(pty)).to end_with("Invalid number of slots\n")
+
     run_command(pty, "create_parking_lot -3\n")
     expect(fetch_stdout(pty)).to end_with("Invalid number of slots\n")
 
@@ -15,21 +18,21 @@ RSpec.describe 'Command Line Functions' do
     expect(fetch_stdout(pty)).to end_with("Invalid number of slots\n")
   end
 
-  it "can create a parking lot", :sample => true do
+  it "creates a parking lot", :sample => true do
     expect(fetch_stdout(pty)).to end_with("Created a parking lot with 3 slots\n")
   end
 
-  it "should fail to create a parking lot if it already exists" do
+  it "fails to create a parking lot if it already exists" do
     run_command(pty, "create_parking_lot 4\n")
     expect(fetch_stdout(pty)).to end_with("Parking lot already exists\n")
   end
 
-  it "can park a car in an available slot" do
+  it "parks a car in an available slot" do
     run_command(pty, "park KA-01-HH-3141 Black\n")
     expect(fetch_stdout(pty)).to end_with("Allocated slot number: 1\n")
   end
 
-  it "can reject parking a car if parking lot is full" do
+  it "fails to park a car if parking lot is full" do
     run_command(pty, "park KA-01-HH-1234 White\n")
     run_command(pty, "park KA-01-HH-3141 Black\n")
     run_command(pty, "park KA-01-HH-9999 White\n")
@@ -38,18 +41,21 @@ RSpec.describe 'Command Line Functions' do
     expect(fetch_stdout(pty)).to end_with("Sorry, parking lot is full\n")
   end
 
-  it "can unpark a car from an occupied slot" do
+  it "unparks a car from an occupied slot" do
     run_command(pty, "park KA-01-HH-3141 Black\n")
     run_command(pty, "leave 1\n")
     expect(fetch_stdout(pty)).to end_with("Slot number 1 is free\n")
   end
 
-  it "can properly fail to unpark a car from an empty slot" do
+  it "fails to unpark a car from an empty slot" do
     run_command(pty, "leave 1\n")
     expect(fetch_stdout(pty)).to end_with("No car in slot number 1\n")
   end
 
-  it "can properly fail to unpark a car from an invalid slot number" do
+  it "fails to unpark a car given an invalid slot number" do
+    run_command(pty, "leave 0\n")
+    expect(fetch_stdout(pty)).to end_with("Invalid slot number\n")
+
     run_command(pty, "leave -1\n")
     expect(fetch_stdout(pty)).to end_with("Invalid slot number\n")
 
@@ -57,7 +63,7 @@ RSpec.describe 'Command Line Functions' do
     expect(fetch_stdout(pty)).to end_with("Invalid slot number\n")
   end
 
-  it "can report status" do
+  it "reports status" do
     run_command(pty, "park KA-01-HH-1234 White\n")
     run_command(pty, "park KA-01-HH-3141 Black\n")
     run_command(pty, "park KA-01-HH-9999 White\n")
@@ -71,7 +77,7 @@ EOTXT
 )
   end
 
-  it "can find registration numbers of all cars of a particular colour" do
+  it "finds registration numbers of all cars of a particular colour" do
     run_command(pty, "park KA-01-HH-1234 White\n")
     run_command(pty, "park KA-01-HH-3141 Black\n")
     run_command(pty, "park KA-01-HH-9999 White\n")
@@ -79,7 +85,7 @@ EOTXT
     expect(fetch_stdout(pty)).to end_with("KA-01-HH-1234, KA-01-HH-9999\n")
   end
 
-  it "can properly fail to find registration numbers of all cars of a particular colour" do
+  it "fails to find registration numbers of all cars of a particular colour" do
     run_command(pty, "registration_numbers_for_cars_with_colour White\n")
     expect(fetch_stdout(pty)).to end_with("Not found\n")
 
@@ -88,7 +94,7 @@ EOTXT
     expect(fetch_stdout(pty)).to end_with("Not found\n")
   end
 
-  it "can find slot numbers for cars with colour" do
+  it "finds slot numbers for cars with colour" do
     run_command(pty, "park KA-01-HH-1234 White\n")
     run_command(pty, "slot_numbers_for_cars_with_colour White\n")
     expect(fetch_stdout(pty)).to end_with("1\n")
@@ -102,7 +108,7 @@ EOTXT
     expect(fetch_stdout(pty)).to end_with("3\n")
   end
 
-  it "can properly fail to find slot numbers for cars with colour" do
+  it "fails to find slot numbers for cars with colour" do
     run_command(pty, "park KA-01-HH-1234 White\n")
     run_command(pty, "slot_numbers_for_cars_with_colour Black\n")
     expect(fetch_stdout(pty)).to end_with("Not found\n")
@@ -112,7 +118,7 @@ EOTXT
     expect(fetch_stdout(pty)).to end_with("Not found\n")
   end
 
-  it "can find slot number in which a car with a given registration number is parked" do
+  it "finds slot number in which a car with a given registration number is parked" do
     run_command(pty, "park KA-01-HH-1234 White\n")
     run_command(pty, "slot_number_for_registration_number KA-01-HH-1234\n")
     expect(fetch_stdout(pty)).to end_with("1\n")
@@ -127,7 +133,7 @@ EOTXT
     expect(fetch_stdout(pty)).to end_with("3\n")
   end
 
-  it "can properly fail to find slot number in which a car with a given registration number is parked" do
+  it "fails to find slot number in which a car with a given registration number is parked" do
     run_command(pty, "park KA-01-HH-1234 White\n")
     run_command(pty, "slot_number_for_registration_number NOT-HE-RE-111\n")
     expect(fetch_stdout(pty)).to end_with("Not found\n")
