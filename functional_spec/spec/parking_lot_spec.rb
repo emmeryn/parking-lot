28d55e026d5524d17b9557090fdede02fa36f97e
Sue Zheng Hao <szhlibrary@gmail.com>
2020-01-19 02:45:01 +0800

    Specs/Parking Lot/park: Test park method of ParkingLot.

--- functional_spec/spec/parking_lot_spec.rb
+++ functional_spec/spec/parking_lot_spec.rb
@@ -26,4 +26,52 @@ RSpec.describe AutomatedTicketingSystem::ParkingLot do
     end
   end
 
+  describe :park do
+    before(:each) do
+      num_of_slots = 3
+      @parking_lot = AutomatedTicketingSystem::ParkingLot.new("#{num_of_slots}")
+    end
+
+    context 'given valid arguments and available capacity' do
+      veh_reg_num = ['REG-NUM', 'REG-NUM-2', 'REG-NUM-3']
+      veh_colour = ['White', 'Black', 'White']
+
+      it 'parks the car in the slot nearest to entry' do
+        @parking_lot.park(veh_reg_num.first, veh_colour.first)
+        expect(@parking_lot.slots.first.car.reg_num).to eql(veh_reg_num.first)
+        expect(@parking_lot.slots.first.car.colour).to eql(veh_colour.first)
+      end
+
+      it 'parks multiple cars' do
+        (0..2).each do |veh_idx|
+          @parking_lot.park(veh_reg_num[veh_idx], veh_colour[veh_idx])
+          expect(@parking_lot.slots[veh_idx].car.reg_num).to eql(veh_reg_num[veh_idx])
+          expect(@parking_lot.slots[veh_idx].car.colour).to eql(veh_colour[veh_idx])
+        end
+      end
+
+      it 'parks a car in the nearest free slot after a car leaves' do
+        (0..2).each do |veh_idx|
+          @parking_lot.park(veh_reg_num[veh_idx], veh_colour[veh_idx])
+        end
+        @parking_lot.leave('1')
+        @parking_lot.park('NEW-REG-NUM', 'New Colour')
+        expect(@parking_lot.slots[0].car.reg_num).to eql('NEW-REG-NUM')
+        expect(@parking_lot.slots[0].car.colour).to eql('New Colour')
+      end
+    end
+
+    context 'given valid arguments but unavailable capacity' do
+      veh_reg_num = ['REG-NUM', 'REG-NUM-2', 'REG-NUM-3']
+      veh_colour = ['White', 'Black', 'White']
+
+      it 'fails to park the car' do
+        (0..2).each do |veh_idx|
+          @parking_lot.park(veh_reg_num[veh_idx], veh_colour[veh_idx])
+        end
+        expect { @parking_lot.park('NEW-REG-NUM', 'New Colour') }
+            .to output("Sorry, parking lot is full\n").to_stdout
+      end
+    end
+  end
 end
